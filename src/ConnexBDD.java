8b13f534696b0f5f53e5cfe0f59e041017447fb0
MikuraAngels <i-release-my-life@live.fr>
2013-11-15 13:03:36 +0100

    Test 2 de commit EGit

--- src/ConnexBDD.java
+++ src/ConnexBDD.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 import java.util.ArrayList;
 
-// Test de commit avec eclipse (EGit) !
+// Test2 de commit avec eclipse (EGit) !
 
 public class ConnexBDD {
 	static Connection connect = null;
